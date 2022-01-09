11729bc3f5d71b7b09bf9e574749c37d9f9d3435
Kshitij Kumar Saini <9968732560kshitijkumar@gmail.com>
2022-01-09 14:06:09 +0530

    First Commit

---
+++ index.js
@@ -0,0 +1,5 @@
+import pizza from "./pizza";
+import "./main.scss"; // will processed by webpack bundler
+
+pizza.pepperoni();
+pizza.bacon();
---
+++ pizza.js
@@ -0,0 +1,8 @@
+export default {
+  pepperoni: function () {
+    console.log("Perpperoni topping added!");
+  },
+  bacon: function () {
+    console.log("bacon topping added");
+  },
+};
